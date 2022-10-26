911b3246d3cdc58ba86f1ac5009e1ea9fdb66d59
Alejandro César Rico Martínez <alejandro.rico@tecsuperiorslp.edu.mx>
2022-10-25 21:47:06 -0500

    Apuntes del día 25/10/2022

---
+++ rumbo_reto_3_05.js
@@ -0,0 +1,42 @@
+var alice=[17,28,30];
+var bob=[99,16,8];
+//var solution=[17+99, 28+16, 30+8]
+var solution=[]
+
+// function read 2 arrays, and return the sum in other array solution
+
+function array_total(a, b)
+{
+    for(var i = 0; i < a.length; i++)
+    {
+        console.log(a[i]);
+        console.log(b[i]);
+    }
+
+}
+
+array_total(alice, bob);
+
+function array_total_0(a, b)
+{
+    for(var i = 0; i < a.length; i++)
+    {
+        console.log("Elemento de Alice ", a[i]);
+        console.log("Elemento de Bob ", b[i]);
+    }
+
+}
+array_total_0(alice, bob);
+
+
+function array_total_1(a, b){
+    for(var i = 0; i < a.length; i++)
+    {
+        solution[i] = a[i] + b[i];
+    }
+
+}
+
+console.log(solution);
+array_total_1(alice, bob);
+console.log(solution);
