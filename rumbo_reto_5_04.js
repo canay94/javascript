5d30e229260e871f9cb56d7ffc341ab16d0a1fd7
Alejandro César Rico Martínez <alejandro.rico@tecsuperiorslp.edu.mx>
2022-11-10 21:34:12 -0600

    Apuntes del día 10/11/2022

--- rumbo_reto_5_04.js
+++ rumbo_reto_5_04.js
@@ -1,57 +1,28 @@
+// https://es.stackoverflow.com/questions/162389/invertir-orden-de-array-multidimensional
 let items = [
   [1,  2,  3,  4],
   [5,  6,  7,  8],
   [9,  10, 11, 12],
   [13, 14, 15, 16 ]
 ];
+var invertida = items.reverse();
+console.log("Invertida ")
+console.log(invertida);
 
-r,c
-0,3*3,2,1,0
-1,3
-2,3
-3,3
-0,2
-1,2*
-2,2
-
-
-
-0,3
-1,3
-2,3
-3,3
-
-
-0,0
-3,1
-2,1*
-1,1
-0,1
-3,2
-2,2
-1,2
-0,2
-
-2,3
-2,2
-2,1
-2,0
-1,3
-1,2
-1,1
-1,0
-
-
-let diagonal1 = 0;
-
+let diagonal2 = 0;
 // find first diagonal
-let r=0;
-let c=0;
-
-for (items.length; r = 0; r <  r++)
+for (var r = 0; r < invertida.length; r++)
 {
-    for(var c = 0; c < items.length; c++)
+    for(var c = 0; c < invertida.length; c++)
     {
-    console.log("Elements array: ", "row: ",r, "column: ",c, "value : ",items[r][c]);
+    console.log("Elements array: ", "row: ",r, "column: ",c, "value : ",invertida[r][c]);
+    if ( r == c)
+    {
+        console.log("Element of diagonal ", invertida[r][c]);
+        diagonal2 = diagonal2 + invertida[r][c];
+        console.log("Acum of diagonal 2: ", diagonal2);
+    }
     }
 }
+var diagonal1 = 34
+console.log(Math.abs(diagonal1 - diagonal2));
